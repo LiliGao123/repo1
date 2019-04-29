cc6583c46e2f99f0de090028958f16198a817e70
LiliGao <1043575262@qq.com>
2019-04-29 11:16:33 +0800

    将maven工程添加到版本库中

---
+++ day04_mybatis_cache/src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,35 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ *
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 根据id查询用户
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 修改用户
+     */
+    void updateUser(User user);
+
+
+
+
+}
